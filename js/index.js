c6ffbf3d2d921e9173964d6050d9c197ea3f8aae
Dowios <sll0219@yahoo.com.tw>
2020-04-14 23:58:16 +0800

    First Try!

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+	$("card").hover(function(){
+                $(this).addClass("enter")
+            },function(){
+                $(this).removeClass("enter")
+            })
+});
